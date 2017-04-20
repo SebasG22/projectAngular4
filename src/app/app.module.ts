31af40ef52c5cdf91e26206a6dfa57dbb19bc439
Sebastian Guevara <rlxsebas@gmail.com>
2017-04-20 18:00:48 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecepiesListComponent } from './recepies/recepies-list/recepies-list.component';
+import { RecepiesDetailComponent } from './recepies/recepies-detail/recepies-detail.component';
+import { RecepiesItemComponent } from './recepies/recepies-list/recepies-item/recepies-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecepiesListComponent,
+    RecepiesDetailComponent,
+    RecepiesItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+      ],
   imports: [
     BrowserModule,
     FormsModule,
